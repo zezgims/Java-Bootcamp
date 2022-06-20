8d7495059143fda1b94b90d76164960d419d382a
Zeynep Ezgi Yalçınkaya <61873515+zezgims@users.noreply.github.com>
2022-06-20 19:21:26 +0300

    Second Homework

--- week2/service/BillService.java
+++ week2/service/BillService.java
@@ -7,12 +7,12 @@ import week2.model.Response;
 
 public abstract class BillService {
 	
-	 public abstract Response payment(int billType, String memberCode, double amount, LocalDate processDate, double balance) throws CustomException;
+    public abstract Response payment(int billType, String memberCode, double amount, LocalDate processDate, double balance) throws CustomException;
 	 
-	 public abstract Response query(int billType, String memberCode);
+    public abstract Response query(int billType, String memberCode);
 	 
-	 public abstract Response query(int billType, String memberCode, String phoneNumber);
+    public abstract Response query(int billType, String memberCode, String phoneNumber);
 	 
-	 public abstract Response cancelPayment(int billType, String memberCode, double amount, LocalDate processDate);
+    public abstract Response cancelPayment(int billType, String memberCode, double amount, LocalDate processDate);
      
 }

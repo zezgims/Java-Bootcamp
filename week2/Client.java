7cc62f5cf062fb2b25424a6d2ad1d8e50de82358
Zeynep Ezgi Yalçınkaya <61873515+zezgims@users.noreply.github.com>
2022-06-20 19:25:06 +0300

    Second Homework

--- week2/Client.java
+++ week2/Client.java
@@ -7,55 +7,55 @@ import week2.service.BillService;
 
 public class Client extends BillService {
 	
-	@Override
-	public Response payment(int billType, String memberCode, double amount, LocalDate processDate, double balance) throws CustomException {
+    @Override
+    public Response payment(int billType, String memberCode, double amount, LocalDate processDate, double balance) throws CustomException {
 		
-		Response response = new Response();
+	Response response = new Response();
 
-		if(amount > balance) {		//Fatura tutarının, üyenin bakiyesinden fazla olduğu durumları kontrol eder ve hata mesajı fırlatır.
-			response.setCode("1");
-			response.setDate(LocalDate.now().toString());
-			throw new CustomException("Yetersiz bakiye!");
-		} 
-		else {
-			response.setCode("0");
-			response.setDate(LocalDate.now().toString());
-			System.out.println("Fatura ödendi.");
-		}
-		
-		return response;
-		
+	if(amount > balance) {		//Fatura tutarÄ±nÄ±n, Ã¼yenin bakiyesinden fazla olduÄŸu durumlarÄ± kontrol eder ve hata mesajÄ± fÄ±rlatÄ±r.
+	    response.setCode("1");
+	    response.setDate(LocalDate.now().toString());
+	    throw new CustomException("Yetersiz bakiye!");
 	} 
-	
-	@Override
-	public Response query(int billType, String memberCode) {
-		
-		if(billType == 2)		//Fatura türü 2'ye eşit olduğu durumlarda internet faturasını sorgular.
-			System.out.println("İnternet faturası sorgulandı.");
-		else if(billType == 3)		//Fatura türü 3'e eşit olduğu durumlarda su faturasını sorgular.
-			System.out.println("Su faturası sorgulandı.");
-		
-		return new Response("0");
-		
+	else {
+	    response.setCode("0");
+	    response.setDate(LocalDate.now().toString());
+	    System.out.println("Fatura Ã¶dendi.");
 	}
+		
+	return response;
+		
+    } 
 	
-	@Override
-	public Response query(int billType, String memberCode, String phoneNumber) {
+    @Override
+    public Response query(int billType, String memberCode) {
 		
-		//Fatura türü 1'e eşit olduğu durumlarda telefon faturasını sorgular.
-		System.out.println("Telefon faturası sorgulandı.");		
+        if(billType == 2)		//Fatura tÃ¼rÃ¼ 2'ye eiit olduÄŸu durumlarda internet faturasÄ±nÄ± sorgular.
+	    System.out.println("Ä°nternet faturasÄ± sorgulandÄ±.");
+	else if(billType == 3)		//Fatura tÃ¼rÃ¼ 3'e eÅŸit olduÄŸu durumlarda su faturasÄ±nÄ± sorgular.
+	    System.out.println("Su faturasÄ± sorgulandÄ±.");
 		
-		return new Response("0");
+	return new Response("0");
 		
-	}
+    }
 	
-	@Override
-	public Response cancelPayment(int billType, String memberCode, double amount, LocalDate processDate) {
+    @Override
+    public Response query(int billType, String memberCode, String phoneNumber) {
 		
-		System.out.println("Ödeme iptal edildi.");
+        //Fatura tÃ¼rÃ¼ 1'e eÃ¾it olduÃ°u durumlarda telefon faturasÃ½nÃ½ sorgular.
+	System.out.println("Telefon faturasÃ½ sorgulandÃ½.");		
 		
-		return new Response("0");
+	return new Response("0");
 		
-	}
+    }
+	
+    @Override
+    public Response cancelPayment(int billType, String memberCode, double amount, LocalDate processDate) {
+		
+        System.out.println("Ã–deme iptal edildi.");
+		
+	return new Response("0");
+		
+    }
 	
 }

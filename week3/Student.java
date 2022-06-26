ad61ae44f795ef523f05cbff54921336bc7223bb
Zeynep Ezgi Yalçınkaya <61873515+zezgims@users.noreply.github.com>
2022-06-26 13:00:20 +0300

    Third Homework

--- week3/Student.java
+++ week3/Student.java
@@ -2,35 +2,35 @@ package week3;
 
 public class Student {
 	
-	private int id;
-	private String name;
-	private double cgpa;
+    private int id;
+    private String name;
+    private double cgpa;
 	
-	Student(int id, String name, double cgpa) {
-		this.id = id;
-		this.name = name;
-		this.cgpa = cgpa;
-	}
+    Student(int id, String name, double cgpa) {
+	this.id = id;
+	this.name = name;
+	this.cgpa = cgpa;
+    }
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
+    public int getId() {
+	return id;
+    }
+    public void setId(int id) {
+	this.id = id;
+    }
 	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+	return name;
+    }
+    public void setName(String name) {
+	this.name = name;
+    }
 	
-	public double getCgpa() {
-		return cgpa;
-	}
-	public void setCgpa(double cgpa) {
-		this.cgpa = cgpa;
-	}
+    public double getCgpa() {
+	return cgpa;
+    }
+    public void setCgpa(double cgpa) {
+	this.cgpa = cgpa;
+    }
 	
 }

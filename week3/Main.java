8f4978841ab9879cc95e7038ec92151e0bf6b98d
Zeynep Ezgi Yalçınkaya <61873515+zezgims@users.noreply.github.com>
2022-06-26 12:56:54 +0300

    Third Homework

--- week3/Main.java
+++ week3/Main.java
@@ -6,36 +6,36 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		
-		Priorities priorities = new Priorities();
+	Priorities priorities = new Priorities();
 		
-		int totalEvents;
-		List<String> events = new ArrayList<String>();
-		List<Student> studentsList;
+	int totalEvents;
+	List<String> events = new ArrayList<String>();
+	List<Student> studentsList;
 		
-		try {
-			Scanner scanner = new Scanner(System.in);		//Metoda gönderilecek verileri kullanıcıdan alır ve events listesine atar.
-			totalEvents = Integer.parseInt(scanner.nextLine()); 
-			for(int i=0; i<totalEvents; i++) {
-				String event;
-				event = scanner.nextLine();
-				events.add(event);
-			}
-			scanner.close();
-		} catch(Exception e) {
-			System.out.println(e);
-		}
-		
-		studentsList = priorities.getStudents(events);		//Kullanıcıdan alınan değerler ile oluşturulan listeyi koşulun gerçekleştirileceği metoda gönderir.
-		
-		if(studentsList.isEmpty()) {		//Metodun geri döndürdüğü listenin boş mu dolu mu olduğunu kontrol eder.
-			System.out.println("EMPTY");
-		}
-		else {		//Liste dolu ise studentList'deki öğrencilerin isimlerini yazdırır.
-			studentsList.forEach(student -> System.out.println(student.getName()));
-		}
-		
+	try {
+	    Scanner scanner = new Scanner(System.in);		//Metoda gÃ¶nderilecek verileri kullanÄ±cÄ±dan alÄ±r ve events listesine atar.
+	    totalEvents = Integer.parseInt(scanner.nextLine()); 
+	    for(int i=0; i<totalEvents; i++) {
+	        String event;
+	        event = scanner.nextLine();
+	        events.add(event);
+	    }
+	    scanner.close();
+	} catch(Exception e) {
+	    System.out.println(e);
 	}
+		
+	studentsList = priorities.getStudents(events);		//KullanÄ±cÄ±dan alÄ±nan deÄŸerler ile oluÅŸturulan listeyi koÅŸulun gerÃ§ekleÅŸtirileceÄŸi metoda gÃ¶nderir.
+		
+	if(studentsList.isEmpty()) {		//Metodun geri dÃ¶ndÃ¼rdÃ¼ÄŸÃ¼ listenin boÅŸ mu dolu mu olduÄŸunu kontrol eder.
+	    System.out.println("EMPTY");
+	}
+	else {		//Liste dolu ise studentList'deki Ã¶ÄŸrencilerin isimlerini yazdÄ±rÄ±r.
+	    studentsList.forEach(student -> System.out.println(student.getName()));
+	}
+		
+    }
 
 }

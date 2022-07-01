61f0771dee030f01fd85f92e001a88e3c998212f
Zeynep Ezgi Yalçınkaya <61873515+zezgims@users.noreply.github.com>
2022-07-01 22:48:43 +0300

    Fourth Homework

--- week4/src/main/java/Main.java
+++ week4/src/main/java/Main.java
@@ -1,27 +1,26 @@
-
 import thread.ReaderThread;
 import thread.WriterThread;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		
-		String filePath = "src/main/resources/students.json";
+	String filePath = "src/main/resources/students.json";
+	
+	ReaderThread readerThread = new ReaderThread(filePath);		//Okuma iÅŸlemi iÃ§in thread oluÅŸturur ve .json dosya yolunu parametre olarak gÃ¶nderir.
+	Thread thread1 = new Thread(readerThread);
 		
-		ReaderThread readerThread = new ReaderThread(filePath);		//Okuma işlemi için thread oluşturur ve .json dosya yolunu parametre olarak gönderir.
-		Thread thread1 = new Thread(readerThread);
+	WriterThread writerThread = new WriterThread();		//Okunan verileri yazdÄ±rmak iÃ§in thread oluÅŸturur.
+	Thread thread2 = new Thread(writerThread);		
 		
-		WriterThread writerThread = new WriterThread();		//Okunan verileri yazdırmak için thread oluşturur.
-		Thread thread2 = new Thread(writerThread);		
-		
-		try {
-			thread1.start();		//Okuma thread'i başlar.
-			Thread.sleep(3000);	
-			thread2.start();		//Yazma thread'i başlar.
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
+	try {
+	    thread1.start();		//Okuma thread'i baÅŸlar.
+	    Thread.sleep(3000);	
+	    thread2.start();		//Yazma thread'i baÅŸlar.
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
 	}
+
+    }
 
 }
